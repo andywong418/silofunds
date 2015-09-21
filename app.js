b3334fd0103aa617d7bc5b5a83427b175a3e72b8
howard611 <howard.tam.95@gmail.com>
2015-09-21 22:50:49 +0800

    Change referred db on heroku

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/db', function (request, response) {
   app.set('view engine', 'ejs');
 
   pg.connect(process.env.DATABASE_URL , function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM items', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
