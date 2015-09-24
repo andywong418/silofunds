0ecb2dca3806ee08baf4fbcfd5bccb455bd7ddd2
howard611 <howard.tam.95@gmail.com>
2015-09-24 09:37:50 +0800

    Setup controller and view for /search

--- controllers/fund.js
+++ controllers/fund.js
@@ -1,5 +1,16 @@
+var models = require('../app/models');
+
 module.exports = {
   index: function(req, res) {
-    res.send('RESPOND with a resource');
+    models.Fund.findAll({ order: 'id ASC' }).then(function(funds) {
+      // convert funds to arr of JSON objects
+      var funds = funds.map(function(fund) {
+        var json = fund.toJSON();
+        console.log(json);
+        return json;
+      });
+
+      res.render('search', { funds: funds });
+    });
   }
 };
