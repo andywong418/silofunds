0f25533f3fbf0710ea427e2cb8f34a91d28a46f7
howard611 <howard.tam.95@gmail.com>
2015-09-26 11:35:39 +0800

    Setup search controller action

--- controllers/fund.js
+++ controllers/fund.js
@@ -2,44 +2,33 @@ var models = require('../models');
 
 module.exports = {
   index: function(req, res) {
-    models.funds.findAll({attributes: ['tags']}).then(function(funds) {
-      //store search string in variable
-    
-     
-      var searchString = req.body.tags;
-      console.log(searchString);
+    models.funds.findAll({ order: 'id ASC' }).then(function(funds) {
       var funds = funds.map(function(fund) {
         var json = fund.toJSON();
-        
         return json;
       });
-      
-      var fundTags = [];
-      
-      for(var i = 0; i<funds.length; i++)
-      {
-           fundTags.push(funds[i].tags);
-       
-      }
-     
-       
-     
-      
-      //filter down to common tags between search string and funds
-     var filteredFunds = fundTags.filter(function(value) {
-       return value.indexOf(searchString) != -1;
-      });
-      
-      //Select the fund objects from filtered down array
-      models.sequelize.query("SELECT * FROM (SELECT * FROM funds WHERE tags in #{filteredFunds}) AS subtable").then(function(funds) {
-      // convert funds to arr of JSON objects
-      return funds;
-   
-    });
-  
 
       res.render('search', { funds: funds });
-    
+    });
+  },
+
+  search: function(req, res) {
+    var searchString = req.body.tags;
+    searchString = searchString.split(', ');
+
+    models.funds.findAll({
+      where: {
+        tags: {
+          $in: searchString
+        }
+      }
+    }).then(function(funds) {
+      var funds = funds.map(function(fund) {
+        var json = fund.toJSON();
+        return json;
+      });
+
+      res.render('search', { funds: funds });
     });
   }
 };
