1f6a5cc6b01e956c37ea1e39d5e80ed20a7ce42f
howard611 <howard.tam.95@gmail.com>
2015-10-01 09:09:35 +0800

    Create descriptionsHelper
    If there is a fund in your db that you haven’t put descriptions for in
    this file, a 500 error will occur for GET /search.

---
+++ helpers/descriptionsHelper.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'Royal Town Planning Institute':
+  'The Royal Towm Planning Institute Trust administers the George Pepler International Award. It is open to overseas candidates who wish to visit the UK, or anyone in the UK who wishes to visit an overseas country, in order to spend time (three to four weeks) studying some particular aspect of town and country planning. This award is not for basic support for students or for postgraduate studies or those working for a doctorate. Such those working for a doctorate. Such applications are unlikely to be successful.',
+
+  'South Square Trust':
+  'One off grants of over £500 pounds can be given to individuals, while collages are also given grants towards their bursary/scholarship funds.',
+
+// The following 'funds' are test funds that exist in my local db
+
+  'hey': 'hey',
+
+  'hey1': 'hey1',
+
+  'hey2': 'hey2'
+}
