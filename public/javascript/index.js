849ca7cb7490ee3c2ac8646f84b7473ac543a93c
howard611 <howard.tam.95@gmail.com>
2015-10-20 11:53:33 +0100

    Cleanup index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,66 +1,51 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
 
-var Scrollview = Backbone.View.extend({
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+
+    event.preventDefault();
+  });
+
+  var Scrollview = Backbone.View.extend({
     el: ".navbar",
+
     initialize: function() {
-        _.bindAll(this, "render", "collapseNavbar");
-        this.render();
+      _.bindAll(this, "render", "collapseNavbar");
+      this.render();
     },
 
     render: function(){
-        $(window).on("scroll", this.collapseNavbar);
+      $(window).on("scroll", this.collapseNavbar);
     },
 
     collapseNavbar: function(){
-        if (this.$el.offset().top > 50) {
-         $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } 
-        else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        }
+      if (this.$el.offset().top > 50) {
+       $(".navbar-fixed-top").addClass("top-nav-collapse");
+      }
+      else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
     }
+  });
 
-})
-    var scrollView = new Scrollview();
+  // Closes the Responsive Menu on Menu Item Click
+  var Menuview = Backbone.View.extend({
+    el: '.navbar-collapse ul li a',
+
+    events:{
+      "click": "navbarToggle"
+    },
     
-// // jQuery to collapse the navbar on scroll
-// $(window).scroll(function() {
-//     if ($(".navbar").offset().top > 50) {
-//         $(".navbar-fixed-top").addClass("top-nav-collapse");
-//     } else {
-//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-//     }
-// });
+    navbarToggle: function(){
+      $('.navbar-toggle:visible').click();
+    }
+  });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  var scrollView = new Scrollview();
+  var menuView = new Menuview();
+
 });
-
-// Closes the Responsive Menu on Menu Item Click
-var Menuview = Backbone.View.extend({
-     el: '.navbar-collapse ul li a',
-     events:{
-        "click": "navbarToggle"
-     },
-     navbarToggle: function(){
-        $('.navbar-toggle:visible').click();
-     }
-
-})
-var menuView = new Menuview();
-// $('.navbar-collapse ul li a').click(function() {
-//     $('.navbar-toggle:visible').click();
-// });
-
-
