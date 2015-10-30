47c23f3d1bcdae4a6ad84b91dc7a4ed41ceada45
howard611 <howard.tam.95@gmail.com>
2015-10-30 17:50:03 +0000

    Change routes from /funds to /admin

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,11 @@
 var index = require('../routes/index');
 var users = require('../routes/users');
 var search = require('../routes/search');
-var funds = require('../routes/funds');
+var admin = require('../routes/admin');
 
 module.exports.initialize = function (app) {
   app.use('/', index);
   app.use('/users', users);
   app.use('/search', search);
-  app.use('/funds', funds);
+  app.use('/admin', admin);
 };
