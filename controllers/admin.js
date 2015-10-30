40a56205f573ecb738367401ddc9148c65f9cec5
howard611 <howard.tam.95@gmail.com>
2015-10-30 20:51:59 +0000

    Enable full functionality of creating fund

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,9 +18,15 @@ module.exports = {
 
   create: function(req, res) {
     var fund = req.body;
+    console.log(fund);
     var title = fund.title;
     var tags = fund.keywords.split(", ");
-    var link = fund.link;
+    var invite = ("invite" in fund);
+    var link = fund.link ? fund.link : null; // Return null if empty string ""
+    var description = fund.description ? fund.description : null;
+    var nationality = fund.nationality ? fund.nationality : null;
+    var religion = fund.religion ? fund.religion : null;
+    var financial_situation = fund.financial_situation ? fund.financial_situation : null;
 
     var parseIfInt = function(string) {
       if (string != '') {
@@ -33,15 +39,38 @@ module.exports = {
     var min_amount = parseIfInt(fund.min_amount);
     var max_amount = parseIfInt(fund.max_amount);
 
+    // If fund is for BOTH genders, gender should be NULL. Similarly for merit / finance field.
+    var gender = null;
+    var merit_or_finance = null;
+
+    if ('male' in fund) {
+      gender = 'male';
+    }
+    if ('female' in fund) {
+      gender = 'female';
+    }
+    if ('merit' in fund) {
+      merit_or_finance = 'merit';
+    }
+    if ('finance' in fund) {
+      merit_or_finance = 'finance';
+    }
+
     models.funds.create({
       title: title,
       tags: tags,
-      invite_only: fund.invite,
+      invite_only: invite,
       link: link,
       minimum_age: min_age,
       maximum_age: max_age,
       minimum_amount: min_amount,
-      maximum_amount: max_amount
+      maximum_amount: max_amount,
+      description: description,
+      nationality: nationality,
+      religion: religion,
+      financial_situation: financial_situation,
+      merit_or_finance: merit_or_finance,
+      gender: gender
     }).then(function(fund) {
       res.redirect('admin');
     });
