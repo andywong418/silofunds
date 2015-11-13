4e86e02117879e29bccabccee242d83e35d8bce1
howard611 <howard.tam.95@gmail.com>
2015-11-13 18:42:10 +0000

    Setup autocomplete in tag bar
    But autocomplete is only for title currently.

--- routes/autocomplete.js
+++ routes/autocomplete.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 router.get('/', function(req,res) {
-  res.send(['I GOTCHU','I GOTCHU SON']);
+  console.log(req.query);
+
+  models.es.search({
+    index: "funds",
+    type: "fund",
+    body: {
+      "query": {
+        "filtered": {
+          "query": {
+            "multi_match": {
+              "query": req.query.term,
+              "fields": ["title.autocomplete"]
+            }
+          }
+        }
+      }
+    }
+  }).then(function(resp) {
+    console.log("This is the response:");
+    console.log(resp);
+    var results = resp.hits.hits.map(function(hit) {
+      console.log("Hit:");
+      console.log(hit);
+
+      return hit._source.title;
+    });
+
+    res.send(results);
+  }, function(err) {
+    console.trace(err.message);
+    res.send({ response: err.message });
+  });
 });
 
 module.exports = router;
