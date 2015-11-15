99c3f8d030253dea492b51244f502a63bf39a186
howard611 <howard.tam.95@gmail.com>
2015-11-15 16:44:09 +0000

    Deprecate /funds and related stuff

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 var index = require('../routes/index');
 var users = require('../routes/users');
 var search = require('../routes/search');
-var funds = require('../routes/funds');
 var signup = require('../routes/signup');
 var login = require('../routes/login');
 var auth = require('../routes/auth');
@@ -12,7 +11,6 @@ module.exports.initialize = function (app) {
   app.use('/', index);
   app.use('/users', users);
   app.use('/search', search);
-  app.use('/funds', funds);
   app.use('/signup', signup);
   app.use('/login', login);
   app.use('/auth', auth);
