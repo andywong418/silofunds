26c252c9aa3d6a75c1bd15191e7a4bee2e46d50b
howard611 <howard.tam.95@gmail.com>
2015-11-18 14:50:32 +0000

    Move clearing jQuery into admin.js

---
+++ public/javascript/admin.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("#clear_gender").click(function(evt) {
+    evt.preventDefault();
+
+    $("#male, #female").prop("checked", false);
+  });
+
+  $("#clear_merit").click(function(evt) {
+    evt.preventDefault();
+
+    $("#merit, #finance").prop("checked", false);
+  });
+});
