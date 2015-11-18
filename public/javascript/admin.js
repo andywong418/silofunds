2119172a3bdabc0c0b755870dc5f3894c7ffce17
howard611 <howard.tam.95@gmail.com>
2015-11-18 15:12:50 +0000

    Move jQuery for prepopulation of radio buttons into admin.js

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -10,4 +10,20 @@ $(function() {
 
     $("#merit, #finance").prop("checked", false);
   });
+
+  try {
+    if(fund.gender != null) {
+      $("#" + fund.gender).prop("checked", true);
+    }
+
+    if(fund.merit_or_finance != null) {
+      $("#" + fund.merit_or_finance).prop("checked", true);
+    }
+
+    if(fund.invite_only) {
+      $("#invite").prop("checked", true);
+    }
+  } catch (e) {
+    console.log("Caught the error ;)");
+  }
 });
