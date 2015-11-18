6ce44ac9dd8d64a08196b9ca1ada0c18be03b443
howard611 <howard.tam.95@gmail.com>
2015-11-18 12:52:52 +0000

    Add routes for EDIT & DELETE

--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', admin.index);
 router.get('/new', admin.new);
 router.post('/', admin.create);
+router.get('/:id/edit', admin.edit);
+router.post('/:id/edit', admin.update);
+router.post('/:id/destroy', admin.destroy);
 
 module.exports = router;
