0d3d1b9f6e034e72a4c6c3abf449757f6292c1e3
howard611 <howard.tam.95@gmail.com>
2015-11-25 12:14:46 +0000

    Create POST route to /upload

--- routes/admin.js
+++ routes/admin.js
@@ -8,5 +8,6 @@ router.post('/', admin.create);
 router.get('/:id/edit', admin.edit);
 router.post('/:id/edit', admin.update);
 router.post('/:id/destroy', admin.destroy);
+router.post('/upload', admin.upload);
 
 module.exports = router;
