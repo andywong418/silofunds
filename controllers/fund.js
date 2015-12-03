acbc020964ffea5d65ba1725efcaeac800923a90
howard611 <howard.tam.95@gmail.com>
2015-12-03 20:44:47 +0000

    Setup searching without filtering from ES

--- controllers/fund.js
+++ controllers/fund.js
@@ -17,7 +17,37 @@ module.exports = {
     var searchString = req.query.tags;
     var searchAge = parseInt(req.query.age);
     var searchAmount = parseInt(req.query.amount);
+
+    models.es.search({
+      index: "funds",
+      type: "fund",
+      body: {
+        "query": {
+          "match_all": {}
+        }
       }
+    }).then(function(resp) {
+      console.log("This is the response:");
+      console.log(resp);
+
+      var funds = resp.hits.hits.map(function(hit) {
+        console.log("Hit:");
+        console.log(hit);
+
+        var title = hit._source.title;
+        var hash = {};
+        
+        hash.title = title;
+
+        return hash;
+      });
+
+      console.log(funds);
+
+      res.render('search', { funds: funds, user: false });
+    }, function(err) {
+      console.trace(err.message);
+      res.render('error');
     });
   }
-}
+};
