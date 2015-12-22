a3f0b556b2104854a5ba4a00f224ec0497e56c37
andywong418 <wong22@hotmail.co.uk>
2015-12-22 11:51:48 +0800

    Finished navbar design for user-profile

--- public/javascript/new-user-profile.js
+++ public/javascript/new-user-profile.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-	
+	var UserNav = Backbone.View.extend({
+        el: "nav",
+
+        initialize: function(){
+        this.searchDropdown();
+      },
+      searchDropdown: function(){
+      	$("#search-dropdown").click(function(e){
+      		e.preventDefault();
+      	});
+      }
+
+  });
+ var userNav = new UserNav();
 
 
 })
