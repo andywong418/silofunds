a06fc3af0e69643c54b06a25cdfbeb3c20eeedf0
andywong418 <wong22@hotmail.co.uk>
2016-01-08 15:06:53 +0000

    Set up view in backbone and changed nav to match signup set up

--- public/javascript/new-user-profile.js
+++ public/javascript/new-user-profile.js
@@ -12,7 +12,36 @@ $(document).ready(function(){
       }
 
   });
- var userNav = new UserNav();
+	var userNav = new UserNav();
+//setting up the user in signup
 
+	var UserModel = Backbone.Model.extend({
+		defaults:{
+			username: "",
+		}
 
+	});
+
+	var UserView = Backbone.View.extend({
+		id: 'user-setup',
+		template: _.template($('#profile-template').html()),
+		render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this; // enable chained calls
+      }
+	})
+	var UserInfo = Backbone.View.extend({
+		el: 'body',
+		initialize: function(){
+			var user = new UserModel({
+          username: user_setup.username
+        });
+
+        var view = new UserView({ model: user });
+
+        this.$el.append(view.render().el);
+		}
+	})
+
+	var userInfo = new UserInfo();
 })
