8c98d67032b8fab437607e76cd9d6acf61499fbe
andywong418 <wong22@hotmail.co.uk>
2016-01-29 10:16:54 +0000

    modified advanced search and added tutorial to use the search bar

--- public/javascript/user-profile.js
+++ public/javascript/user-profile.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 //setting up page view
-console.log(user);
+
 	var UserModel = Backbone.Model.extend({
 		defaults: {
 			name: "",
@@ -32,8 +32,6 @@ console.log(user);
 			var birthDate = new Date(newDate).getTime();
 			var nowDate = new Date().getTime();
 			var age = Math.floor((nowDate - birthDate) / 31536000000 );
-			console.log(age);
-			console.log(newUser);
 			var user_model = new UserModel({
 				name: user.username,
 				age: age,
@@ -44,31 +42,72 @@ console.log(user);
 			});
 			var view = new UserView({model: user_model});
 			this.$el.append(view.render().el);
-			var advanced = false;
-			$("#advanced-search").toggle(advanced);
-
-			$("#search-dropdown").click(function(e){
-      		e.preventDefault();
+			var advanced = true;
+			var advanced_2 = true;
+			$("#advanced-search").toggle(false);
+			$("#advanced-search-2").toggle(false);
+			$("#grants").click(function(){
       		$("#advanced-age").attr("value", age);
-      		if(advanced == false){
-      			advanced = true;
-      		}
-      		else{
-      			advanced = false;
-      		}
-      		$("#advanced-search").toggle(advanced);
+      		$("#advanced-search").toggle(true);
+      		$("#grants span").css("display","inline");
+      		$("#users span").css("display","none");
+      		$("#text_search").attr("name", "fund_tags");
+      		advanced = false;
+      		return true;
       	});
+			
+			$("#users").click(function(){
+				$("#advanced-age-2").attr("value", age);
+  
+      		$("#advanced-search-2").toggle(true);
+
+      		$("#users span").css("display","inline");
+      		$("#grants span").css("display","none");
+      		$("#text_search").attr("name", "user_tags")
+      		advanced_2 = false; 
+			});
+			$(document).click(function(e) {
+				console.log(e.target.closest('#grants'));
+				console.log($(e.target).closest('#advanced-search').length);
+		    if ( $(e.target).closest('#advanced-search').length == 0 && e.target.closest('#grants') === null) {
+		        // cancel highlighting 
+		        console.log("Is it still getting in here?");
+		        e.preventDefault();
+		       
+		        $("#advanced-search").toggle(false);      	
+		      	
+		    }
+		    else{
+		      		return true;
+		      	}
+
+		    if ( $(e.target).closest('#advanced-search-2').length == 0 && e.target.closest('#users') === null) {
+	        // cancel highlighting 
+	        console.log("Is it still getting in here?");
+	        e.preventDefault();
+	       
+	        $("#advanced-search-2").toggle(false);      	
+      	
+		    }
+		    else{
+		      		return true;
+		    }
+
+});
+			
+			
+		
+			
 			this.workDisplay();
+			this.newUser();
 		},
 		workDisplay: function(){
 			var pastWork = user.past_work;
 			for( var i = 0; i < pastWork.length; i++){
 				var seekingExtension = pastWork[i].split(".");
-				var extension = seekingExtension[3];
-				console.log(extension);
+				var extension = seekingExtension[3];			
 				var seekingFilename = pastWork[i].split("/");
 				var filename = seekingFilename[4];
-				console.log(filename);
 				if(extension == "pdf"){
 					$("#work-display").append("<span><i class = 'fa fa-file-pdf-o pdf-file' ><a href = '" + pastWork[i] + "'>"+ filename + "</a></i></span>");
 				}
@@ -97,6 +136,49 @@ console.log(user);
 			}
 		},
 		newUser: function(){
+			var allowed = false;
+			$(".get-started").click(function(){
+				if(newUser){
+					$("#user-modal").css("display", "inline");
+					$('html, body').animate({
+    		    scrollTop: 0
+			    }, 2000);
+			    $("#instruction-footer").css("display", "inline");
+			    $(".instruction-pointer").css("display","inline")
+			    allowed = true;
+				}
+			});
+				
+				var counter = 0;
+				var down = {};
+			  $(document).keydown(function(e){
+			    	var key = (e.keyCode ? e.keyCode : e.which);
+			    	if(key == '13' && allowed){		
+			    			if(down['13'] == null){	  
+			    				e.preventDefault();
+			    				if(counter == 0){
+			    				
+						    		$(".instruction-pointer").css("display","none");
+						    		$(".instruction-pointer-2").css("display","inline");	
+						    		counter++;
+						    		return;
+					    		}
+					    		if(counter == 1){
+					    			$(".instruction-pointer-2").css("display", "none");
+					    			$("#instruction-footer").css("display","none");
+					    			$("#user-modal").css("display","none");
+					    			$("#text_search").focus();
+					    			counter = 0;
+					    		}
+				    		}
+			    	}
+						
+			  });
+			$(document).keyup(function(event) {
+		     var keycode = (event.keyCode ? event.keyCode : event.which);
+		     down[keycode] = null;
+			});
+
 
 		}
 
