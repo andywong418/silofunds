33bb17b60848f99aedd13229980c0e3b335d5e7b
Howard <howard.tam.95@gmail.com>
2016-02-01 21:21:39 +0000

    Setup authentication for admin section

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,26 @@ var auth = require('../routes/auth');
 var admin = require('../routes/admin');
 var autocomplete = require('../routes/autocomplete');
 var validation = require('../routes/validation');
+var basicAuth = require('basic-auth');
+
+var auth = function (req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Admin Authorization Required');
+    return res.send(401);
+  }
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  }
+
+  if (user.name === 'foo' && user.pass === 'bar') {
+    return next();
+  } else {
+    return unauthorized(res);
+  }
+};
 
 module.exports.initialize = function (app) {
   app.use('/', index);
@@ -15,7 +35,7 @@ module.exports.initialize = function (app) {
   app.use('/signup', signup);
   app.use('/login', login);
   app.use('/auth', auth);
-  app.use('/admin', admin);
+  app.use('/admin', auth, admin);
   app.use('/autocomplete', autocomplete);
   app.use('/validation', validation);
 };
