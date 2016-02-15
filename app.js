27d8f41ba1dae1dbd4ed6c6f463ecc85642c30c1
Howard <howard.tam.95@gmail.com>
2016-02-15 22:10:46 +0000

    Setup flash messaging

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(logger('dev'));
 app.use(cookieParser('keyboard cat'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(flash());
 
 var redisHost = 'localhost';
 var redisPort = 6379;
@@ -67,7 +66,11 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 // Load routes
 routes.initialize(app);
