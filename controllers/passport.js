1e5d8504b230ec628fcbfc3bf784d27f2c97203e
Howard <howard.tam.95@gmail.com>
2016-02-16 00:03:56 +0000

    Fix passport FB cb url

--- controllers/passport.js
+++ controllers/passport.js
@@ -44,7 +44,7 @@ module.exports = function(passport) {
     passport.use('facebook', new FacebookStrategy({
       clientID: '506830149486287',
       clientSecret: '45b00c46d1cf3d9396fd24fe99ea0e3d',
-      callbackURL: "http://www.localhost:3001/auth/facebook/callback",
+      callbackURL: "https://silofunds.herokuapp.com/auth/facebook/callback",
       profileFields : ['id', 'displayName', 'emails']
     }, function(accessToken, refreshToken, profile, done) {
 
