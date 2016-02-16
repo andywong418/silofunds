dba3c4378d480ddc718614b4166d76ff363d4e62
Howard <howard.tam.95@gmail.com>
2016-02-16 00:19:12 +0000

    Trying to debug FB auth

--- controllers/passport.js
+++ controllers/passport.js
@@ -45,10 +45,11 @@ module.exports = function(passport) {
       clientID: '506830149486287',
       clientSecret: '45b00c46d1cf3d9396fd24fe99ea0e3d',
       callbackURL: "https://silofunds.herokuapp.com/auth/facebook/callback",
-      profileFields : ['id', 'displayName', 'emails']
+      profileFields : ['id', 'displayName', 'email']
     }, function(accessToken, refreshToken, profile, done) {
 
       process.nextTick(function() {
+        console.log(profile);
         models.users.find({
           where: {email: profile.emails[0].value}
         }).then(function(user) {
