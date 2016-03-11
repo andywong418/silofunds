79fd347aa7d492d684a9581f6b4a668d1310674b
andywong418 <wong22@hotmail.co.uk>
2016-03-11 17:05:05 +0000

    Fix Howard's shit

--- routes/autocomplete.js
+++ routes/autocomplete.js
@@ -31,38 +31,6 @@ router.get('/', function(req,res) {
     console.trace(err.message);
     res.send({ response: err.message });
   });
-
-  // models.es.search({
-  //   index: "funds",
-  //   type: "fund",
-  //   body: {
-  //     "size": 5,
-  //     "query": {
-  //       "filtered": {
-  //         "query": {
-  //           "multi_match": {
-  //             "query": req.query.term,
-  //             "fields": ["tags","title.autocomplete"]
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }).then(function(resp) {
-  //   console.log("This is the response:");
-  //   console.log(resp);
-  //   var results = resp.hits.hits.map(function(hit) {
-  //     console.log("Hit:");
-  //     console.log(hit);
-
-  //     return hit._source.title;
-  //   });
-
-  //   res.send(results);
-  // }, function(err) {
-  //   console.trace(err.message);
-  //   res.send({ response: err.message });
-  // });
 });
 
 module.exports = router;
