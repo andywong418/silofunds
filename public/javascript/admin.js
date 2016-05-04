53430ed6e8fac599442083ba88d259780035b209
howard611 <howard.tam.95@gmail.com>
2016-05-04 21:37:57 +0100

    Add back to top button

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -1,4 +1,22 @@
 $(function() {
+  // Back to Top button
+  var amountScrolled = 300;
+  $(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+      $('a.back-to-top').fadeIn('slow');
+    } else {
+      $('a.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('a.back-to-top').click(function() {
+  	$('html, body').animate({
+  		scrollTop: 0
+  	}, 700);
+    
+  	return false;
+  });
+
   $("#clear_gender").click(function(evt) {
     evt.preventDefault();
 
