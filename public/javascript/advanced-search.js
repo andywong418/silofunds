aca1eca3493ae7143108de6da4963762d1fa7955
howard611 <howard.tam.95@gmail.com>
2016-07-07 11:07:35 +0800

    Enable toggle functionality of category button

--- public/javascript/advanced-search.js
+++ public/javascript/advanced-search.js
@@ -16,12 +16,15 @@ $(document).ready(function(){
   function split( val ) {
       return val.split(" ");
   };
-  $("#grants").click(function(){
-      $("#advanced-search").slideDown();
-      $("#advanced-search-2").toggle(false);
-      $("#grants span").css("display","inline");
-      $("#users span").css("display","none");
-      advanced = false;
+
+  $("a.category").click(function(event) {
+    var currentTarget = event.target;
+
+    if (currentTarget.id === "category-funding") {
+      $("button#category").html("Funding <span class='caret'></span>");
+      $("form.search_form .input-group-btn").removeClass("open");
+      $("#advanced-search .tab-pane#advs-user, #advanced-search li#search-for-users").removeClass("active");
+      $("#advanced-search .tab-pane#advs-funding, #advanced-search li#search-for-funding").addClass("active");
       $("#search-form").attr('action', '/results');
       $("#text_search").attr('placeholder', 'Keywords - Subject, University, Degree level');
       $("input#text_search" ).autocomplete({
@@ -43,9 +46,37 @@ $(document).ready(function(){
           return false;
         }
       });
+    } else {
+      $("button#category").html("Users <span class='caret'></span>");
+      $("form.search_form .input-group-btn").removeClass("open");
+      $("#advanced-search .tab-pane#advs-user, #advanced-search li#search-for-users").addClass("active");
+      $("#advanced-search .tab-pane#advs-funding, #advanced-search li#search-for-funding").removeClass("active");
+      $("#search-form").attr('action', '/results/users');
+      $("#text_search").attr('placeholder', 'Search for users by name or by interests');
+      $("input#text_search" ).autocomplete({
+        source: "../autocomplete/users",
+        minLength: 1,
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join(" ");
+          return false;
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        }
+      });
+    }
 
-      return true;
-    });
+    return false;
+  });
+
   $('a#advs-link').click(function(){
     $("#advanced-search").slideDown();
     advanced = false;
@@ -80,34 +111,6 @@ $(document).ready(function(){
     currentTarget.submit();
   });
 
-  $("#users").click(function(){
-      $("#advanced-search-2").toggle(true);
-      $("#advanced-search").toggle(false);
-      $("#users span").css("display","inline");
-      $("#grants span").css("display","none");
-      advanced_2 = false;
-      $("#search-form").attr('action', '/results/users');
-      $("#text_search").attr('placeholder', 'Search for users by name or by interests');
-      $("input#text_search" ).autocomplete({
-        source: "../autocomplete/users",
-        minLength: 1,
-        select: function( event, ui ) {
-          var terms = split( this.value );
-          // remove the current input
-          terms.pop();
-          // add the selected item
-          terms.push( ui.item.value );
-          // add placeholder to get the comma-and-space at the end
-          terms.push( "" );
-          this.value = terms.join(" ");
-          return false;
-        },
-        focus: function() {
-          // prevent value inserted on focus
-          return false;
-        }
-      });
-  });
   $(document).click(function(e) {
     if ( $(e.target).closest('#advanced-search').length == 0 && e.target.closest('#grants') === null && e.target.closest('#advs-link') === null && e.target.closest('#search_button') === null && e.target.closest('#text_search') === null) {
         $("#advanced-search").toggle(false);
