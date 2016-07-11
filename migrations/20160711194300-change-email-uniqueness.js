cc14d8288a35f5b67f06a7174225dfcbe0465371
howard611 <howard.tam.95@gmail.com>
2016-07-11 20:14:09 +0100

    lol gg cheeky

--- migrations/20160711194300-change-email-uniqueness.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-  up: function (queryInterface, Sequelize) {
-    return queryInterface.sequelize.query('ALTER TABLE "funds" DROP CONSTRAINT "email_unique_idx"');
-  },
-  down: function (queryInterface, Sequelize) {
-    return queryInterface.sequelize.query('ALTER TABLE "funds" ADD CONSTRAINT "email_unique_idx" UNIQUE ("email")');
-  }
-};
