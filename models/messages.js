150125450cde1e447694c5b542b957d34b4f5e50
howard611 <howard.tam.95@gmail.com>
2016-07-20 11:56:04 +0100

    Messages migration

---
+++ models/messages.js
@@ -0,0 +1,54 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes){
+	var Message = sequelize.define("messages", {
+    id: {
+      type: DataTypes.INTEGER,
+      field: 'id',
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true
+    },
+    user_from: {
+      type: DataTypes.INTEGER,
+      field: 'user_from'
+    },
+    user_to: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+      field: 'user_to'
+    },
+		message: {
+			type: DataTypes.TEXT,
+			field: 'message'
+		},
+    created_at: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+      defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      field: 'updated_at',
+      defaultValue: DataTypes.NOW
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      field: 'deleted_at'
+    }
+  }, {
+		timestamps: true,
+    underscored: true,
+    paranoid: true,
+
+    classMethods: {
+      associate: function(models) {
+        Message.belongsTo(models.users, {
+          onDelete: 'CASCADE',
+          as: 'Message',
+          foreignKey: 'id'
+        });
+      }
+    }
+	});
+  return Message;
+};
