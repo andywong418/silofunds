802e0ebc99e30364cbfcf1dfc13e0ebb93d29da9
howard611 <howard.tam.95@gmail.com>
2016-07-21 15:30:28 +0100

    Add today helpers

--- public/javascript/messages.js
+++ public/javascript/messages.js
@@ -75,15 +75,53 @@ $(document).ready(function() {
     var arr_of_messages = data.bulk_messages;
     $('#messages').empty();
 
+
+
+    var dateNow = new Date();
+    var dateYesterday = new Date();
+    dateYesterday.setDate(dateNow.getDate() - 1);
+    console.log("dateNow");
+    console.log(dateNow);
+    console.log("dateYesterday");
+    console.log(dateYesterday);
+
+
+    var appendTodayToFirstMessage = 0;
+
     for (var i=0; i < arr_of_messages.length; i++) {
       message = arr_of_messages[i];
       var userToUsername = $('.list-group-item.active h5').html();
 
-      if (message.user_from === user.id) {
-        $('#messages').append('<div class="user_from col-md-12"><img class="col-md-1" src=' + data.userFrom.profile_picture + ' /><div class="col-md-9"><span class="user_from">' + data.userFrom.username + ':</span><li>' + message.message + '</li></div><div class="col-md-2 timestamp">' + retrieveTime(message.created_at) + '</div></div><br>');
+      var dateOfMessage = retrieveDate(message.created_at);
+      var messageSentToday = dateOfMessage === dateNow.toDateString();
+
+
+
+      if (messageSentToday && appendTodayToFirstMessage === 0) {
+        console.log('message was sent today');
+        console.log(message.message);
+
+        if (message.user_from === user.id) {
+          $('#messages').append('<div class="date-of-message col-md-12"><span class="date-of-message">Today</span></div>');
+          $('#messages').append('<div class="user_from col-md-12"><img class="col-md-1" src=' + data.userFrom.profile_picture + ' /><div class="col-md-9"><span class="user_from">' + data.userFrom.username + ':</span><li>' + message.message + '</li></div><div class="col-md-2 timestamp">' + retrieveTime(message.created_at) + '</div></div><br>');
+        } else {
+          $('#messages').append('<div class="user_to col-md-12"><img class="col-md-1" src=' + data.userTo.profile_picture + ' /><div class="col-md-9"><span class="user_to">' + data.userTo.username + ':</span><li>' + message.message + '</li></div><div class="col-md-2 timestamp">' + retrieveTime(message.created_at) + '</div></div><br>');
+        }
+
+        appendTodayToFirstMessage++;
       } else {
-        $('#messages').append('<div class="user_to col-md-12"><img class="col-md-1" src=' + data.userTo.profile_picture + ' /><div class="col-md-9"><span class="user_to">' + data.userTo.username + ':</span><li>' + message.message + '</li></div><div class="col-md-2 timestamp">' + retrieveTime(message.created_at) + '</div></div><br>');
+        if (message.user_from === user.id) {
+          $('#messages').append('<div class="user_from col-md-12"><img class="col-md-1" src=' + data.userFrom.profile_picture + ' /><div class="col-md-9"><span class="user_from">' + data.userFrom.username + ':</span><li>' + message.message + '</li></div><div class="col-md-2 timestamp">' + retrieveTime(message.created_at) + '</div></div><br>');
+        } else {
+          $('#messages').append('<div class="user_to col-md-12"><img class="col-md-1" src=' + data.userTo.profile_picture + ' /><div class="col-md-9"><span class="user_to">' + data.userTo.username + ':</span><li>' + message.message + '</li></div><div class="col-md-2 timestamp">' + retrieveTime(message.created_at) + '</div></div><br>');
+        }
       }
+
+
+
+
+
+
     }
 
     $('#messages-list').scrollTop($("#messages-list")[0].scrollHeight);
@@ -92,6 +130,12 @@ $(document).ready(function() {
   /* -------------- */
 
 });
+
+function checkDate() {
+  dateNow = new Date();
+
+
+}
 
 function retrieveDate(date) {
   date = date.split('T')[0];
