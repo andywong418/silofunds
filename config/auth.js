23ea5565ba8a527d6ddbe90cfb5ad68b7f21302d
James Morrill <james.morrill.6@gmail.com>
2016-07-21 19:25:37 +0100

    Deleted unnessecary file

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'facebookAuth': {
-    clientID: '506830149486287',
-    clientSecret: '45b00c46d1cf3d9396fd24fe99ea0e3d',
-    callbackURL: "https://silofunds.herokuapp.com/auth/facebook/callback",
-    profileFields : ['id', 'displayName', 'email']
-  }
-}
