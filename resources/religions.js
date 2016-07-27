a7c8888fb59aecb4efb2fe9cde47664b50277f89
howard611 <howard.tam.95@gmail.com>
2016-07-27 14:06:26 +0100

    Add resources for degrees religions and universities

---
+++ resources/religions.js
@@ -0,0 +1,3 @@
+var religions = ["Atheism", "Baha'i", "Buddhism", "Candomblé", "Christianity", "Hinduism", "Islam", "Jainism", "Jehovah's Witnesses", "Judaism", "Mormonism", "Paganism", "Rastafari", "Santeria", "Shinto", "Sikhism", "Spiritualism", "Taoism", "Unitarianism", "Zoroastrianism"];
+
+module.exports = religions;
