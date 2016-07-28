6ca498d63051272a5b50f43eed51c187e8cd44ca
howard611 <howard.tam.95@gmail.com>
2016-07-28 23:08:56 +0100

    Remove user organization accounts inter-blockage

--- routes/index.js
+++ routes/index.js
@@ -45,25 +45,27 @@ router.post('/register', signup.subscribe, passport.authenticate('registrationSt
 router.get('/registerSplit', users.registerSplitterGET)
 
 
-// If a user is logged in, then they should not be able to access fund pages, and vice versa
-router.get(/user/, function(req, res, next){
-  console.log(req.user)
-  if(req.user.organisation_or_user !== null) {
-    res.render(error);
-    res.end()
-  } else {
-    next();
-  }
-})
-router.get(/organisation/, function(req, res, next){
-  console.log(req.user)
-  if(req.user.organisation_or_user == null) {
-    res.render(error);
-    res.end()
-  } else {
-    next();
-  }
-})
+// NOTE:If a user is logged in, then they should not be able to access fund pages, and vice versa
+// router.get(/user/, function(req, res, next){
+//   console.log(req.user)
+//   if(req.user.organisation_or_user !== null) {
+//     res.render(error);
+//     res.end()
+//   } else {
+//     next();
+//   }
+// })
+
+// NOTE: without below, an organisation can get onto user page.
+// router.get(/organisation/, function(req, res, next){
+//   console.log(req.user)
+//   if(req.user.organisation_or_user == null) {
+//     res.render(error);
+//     res.end()
+//   } else {
+//     next();
+//   }
+// })
 
 // *** Password reset
 // GET forgotten password page (added some possible flash messages)
