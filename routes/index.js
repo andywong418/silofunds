4ca4a585e15392afdea62ed3e60ca2a9bb23a7c4
JimmyMorrill <Jimbo@Jamess-MBP.home>
2016-07-28 23:08:41 +0100

    commented out /organisation/, /user/ stuff

--- routes/index.js
+++ routes/index.js
@@ -38,25 +38,26 @@ router.post('/register', signup.subscribe, passport.authenticate('registrationSt
 router.get('/registerSplit', users.registerSplitterGET)
 
 
+// NOTE: This needs changing
 // If a user is logged in, then they should not be able to access fund pages, and vice versa
-router.get(/user/, function(req, res, next){
-  console.log(req.user)
-  if(req.user.organisation_or_user !== null) {
-    res.render(error);
-    res.end()
-  } else {
-    next();
-  }
-})
-router.get(/organisation/, function(req, res, next){
-  console.log(req.user)
-  if(req.user.organisation_or_user == null) {
-    res.render(error);
-    res.end()
-  } else {
-    next();
-  }
-})
+// router.get(/user/, function(req, res, next){
+//   console.log(req.user)
+//   if(req.user.organisation_or_user !== null) {
+//     res.render(error);
+//     res.end()
+//   } else {
+//     next();
+//   }
+// })
+// router.get(/organisation/, function(req, res, next){
+//   console.log(req.user)
+//   if(req.user.organisation_or_user == null) {
+//     res.render(error);
+//     res.end()
+//   } else {
+//     next();
+//   }
+// })
 
 // *** Password reset
 // GET forgotten password page (added some possible flash messages)
