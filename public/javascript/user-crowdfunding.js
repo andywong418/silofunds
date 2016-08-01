d863057acc653b53e04392ae1e6b13ac46e0a958
howard611 <howard.tam.95@gmail.com>
2016-08-01 17:53:37 +0100

    Basic functionality for user donation

--- public/javascript/user-crowdfunding.js
+++ public/javascript/user-crowdfunding.js
@@ -47,4 +47,50 @@ $(document).ready(function(){
   })
   var router = new Router();
   Backbone.history.start();
-})
+
+  $('#donate').click(function() {
+    $('#donate').animate({ opacity: 0 }, 100, function() {
+      $('#donate').addClass('hidden');
+
+      $('#donate-amount').removeClass('hidden');
+    });
+
+    $('#donate-amount').animate({ opacity: 1}, 300, "easeInOutExpo", function() {
+
+    });
+  });
+
+
+
+  // Stripe
+
+  var handler = StripeCheckout.configure({
+      key: 'pk_test_APDW1SKRsKrZAh5sf0q1ur8r',
+      billingAddress: true,
+      zipCode: true,
+      image: '/images/silo-transparent-square.png',
+      locale: 'auto',
+      token: function(token) {
+
+      }
+    });
+
+    $('#donate-amount a').on('click', function(e) {
+      // Open Checkout with further options:
+      var amount = $('input#donate-amount').val() * 100;
+
+      handler.open({
+        name: 'Silo',
+        description: '2 widgets',
+        currency: "gbp",
+        panelLabel: "Donate",
+        amount: amount
+      });
+      e.preventDefault();
+    });
+
+    // Close Checkout on page navigation:
+    $(window).on('popstate', function() {
+      handler.close();
+    });
+});
