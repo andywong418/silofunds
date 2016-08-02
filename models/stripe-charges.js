821293ca21f82d2163da665a37ddce57ec81c193
howard611 <howard.tam.95@gmail.com>
2016-08-02 21:45:54 +0100

    Create migration for stripe_charges

---
+++ models/stripe-charges.js
@@ -0,0 +1,88 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+	var StripeCharge = sequelize.define("stripe_charges", {
+    id: {
+      type: DataTypes.INTEGER,
+      field: 'id',
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true
+    },
+    charge_id: {
+      type: DataTypes.STRING(100),
+      field: 'charge_id'
+    },
+    amount: {
+      type: DataTypes.DOUBLE,
+      field: 'amount'
+    },
+    application_fee: {
+      type: DataTypes.DOUBLE,
+      field: 'application_fee'
+    },
+    balance_transaction: {
+      type: DataTypes.STRING(100),
+      field: 'balance_transaction'
+    },
+    captured: {
+      type: DataTypes.BOOLEAN,
+      field: 'captured'
+    },
+    customer_id: {
+      type: DataTypes.STRING(100),
+      field: 'customer_id'
+    },
+    description: {
+      type: DataTypes.STRING(100),
+      field: 'description'
+    },
+    destination_id: {
+      type: DataTypes.STRING(100),
+      field: 'destination_id'
+    },
+    livemode: {
+      type: DataTypes.BOOLEAN,
+      field: 'livemode'
+    },
+    paid: {
+      type: DataTypes.BOOLEAN,
+      field: 'paid'
+    },
+    status: {
+      type: DataTypes.TEXT,
+      field: 'status'
+    },
+    transfer_id: {
+      type: DataTypes.STRING(100),
+      field: 'transfer_id'
+    },
+    source_id: {
+      type: DataTypes.STRING(100),
+      field: 'source_id'
+    },
+    source_address_line1_check: {
+      type: DataTypes.STRING(100),
+      field: 'source_address_line1_check'
+    },
+    source_address_zip_check: {
+      type: DataTypes.STRING(100),
+      field: 'source_address_zip_check'
+    },
+    source_cvc_check: {
+      type: DataTypes.STRING(100),
+      field: 'source_cvc_check'
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+      defaultValue: DataTypes.NOW
+    },
+  }, {
+		timestamps: true,
+    underscored: true,
+    paranoid: true,
+	});
+
+  return StripeCharge;
+};
