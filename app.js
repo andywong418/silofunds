3f2b0739463e0b454c28df080f8cd590ad900cc7
howard611 <howard.tam.95@gmail.com>
2016-08-07 16:07:06 +0100

    Run mocha tests using npm test with silent HTTP requests
    silento

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+if (process.argv.indexOf('--silent-http') === -1) {
+  app.use(logger('dev'));
+}
 app.use(cookieParser('keyboard cat'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
