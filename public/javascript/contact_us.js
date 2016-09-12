e8949c4e461c263b55bc4f348f88e864f6a5ae09
JimmyMorrill <Jimbo@Jamess-MBP.lan>
2016-09-12 19:01:41 +0100

    partials added and css'ed (for mobile) for users and people non logged in

---
+++ public/javascript/contact_us.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#user_box').click(function() {
+    $('.non-user').hide();
+    $('#user_contact').show();
+  })
+  $('#organisation_box').click(function() {
+    $('.non-user').hide();
+    $('#organisation_contact').show();
+  })
+
+  $('.textarea').focus()
+})
