f8fc996e27fc52070e64450867bd91f8aef0268d
JimmyMorrill <Jimbo@Jamess-MBP.lan>
2016-09-13 15:08:15 +0100

    resized for 1000px and 800px +

--- public/javascript/contact_us.js
+++ public/javascript/contact_us.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Jquery for a non logged in user.
   $('#user_box').click(function() {
     $('.non-user').hide();
     $('#user_contact').show();
@@ -15,4 +16,19 @@ $(document).ready(function() {
     var message = $('.textarea').text()
     $('#message').val(message)
   })
+
+  makeButtonsInline();
+  $(window).resize(function() {
+    makeButtonsInline();
+  })
 })
+
+
+function makeButtonsInline() {
+  var contactFormHeight = $('.contact_form').height() // Minus padding top/bottom
+  var messageFormHeight = $('#message_form').height()
+  if(contactFormHeight !== messageFormHeight) {
+    var buttonMargin = (contactFormHeight - messageFormHeight)
+    $('.send').css('margin-top', buttonMargin)
+  }
+}
