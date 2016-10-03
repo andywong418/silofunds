1e495e6659d203878ba486455c9f87c63a9f3579
JimmyMorrill <Jimbo@Jamess-MBP.home>
2016-10-03 12:09:25 +0100

    commit

--- controllers/signup.js
+++ controllers/signup.js
@@ -471,12 +471,10 @@ module.exports = {
 							user_id: userId
 						}).then(function(row) {
 							if(heard_from !== 'other' && heard_from !== '') {
-								console.log('fuck andy')
 								row.update({[heard_from]: true}).then(function() {
 									res.send(user)
 								})
 							} else if (heard_from == 'other') {
-								console.log('fuck andy')
 								row.update({[heard_from]: req.body.heard_other}).then(function() {
 									res.send(user)
 								})
