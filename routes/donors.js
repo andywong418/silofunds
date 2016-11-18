26f68cd795de916b47ea1e8dd69eeee9c610e32a
JimmyMorrill <Jimbo@Jamess-MacBook-Pro-2.local>
2016-11-18 10:53:59 +0000

    more name changes

--- routes/donors.js
+++ routes/donors.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var models = require('../models');
-var alumni = require('../controllers/alumni');
+var donors = require('../controllers/donors');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 require('../controllers/passport/strategies')(passport);
 var router = express.Router();
 
-router.get('/profile', alumni.profile)
+router.get('/profile', donors.profile)
+router.get('/signup', donors.signup)
 
 module.exports = router
