cbe54997aa55b6ff6a1a4b824d4bdb5dea754655
JimmyMorrill <Jimbo@Jamess-MacBook-Pro-2.local>
2016-11-30 17:03:46 +0000

    routing and copying

---
+++ controllers/donors.js
@@ -0,0 +1,52 @@
+var models = require('../models');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+require('./passport/strategies')(passport);
+var passportFunctions = require('./passport/functions');
+
+module.exports = {
+  profile: function(req, res) {
+    passportFunctions.ensureAuthenticated(req, res, function() {
+      res.render('donor/profile', {user: req.user});
+    });
+  },
+
+  register: function(req, res) {
+    res.render('donor/register')
+  },
+
+  // register: function(req, res) {
+  //   var errorInformation = null;
+  //   if(req.session.flash) {
+  //     if(req.session.flash.length == 0 || req.session.flash.length == undefined) {
+  //       // do nothing
+  //     } else {
+  //       errorInformation = req.session.flash.errorInformation[0].split(',')
+  //     }
+  //   }
+  //   if(errorInformation !== null) {
+  //     var errorInformation = req.session.flash.errorInformation[0].split(',')
+  //     var error = errorInformation[0]
+  //     var stripe_id = parseInt(errorInformation[1])
+  //     var user_id = parseInt(errorInformation[2])
+  //     res.render('donor/register', {stripe_id: stripe_id, user_id: user_id, error: error})
+  //   } else {
+  //     req.session.flash = []
+  //     req.flash = []
+  //     res.render('donor/register')
+  //   }
+  // },
+  //
+  // transaction_signup: function(req, res) {
+  //   var stripe_id = req.body.stripe_id
+  //   var user_id = req.body.user_id
+  //   res.render('donor/register', {user_id: user_id, stripe_id: stripe_id})
+  // },
+
+  logout: function(req, res) {
+    res.cookie('remember_me', '', {expires: new Date(1), path: '/'});
+    req.flash('logoutMsg', 'Successfully logged out');
+    req.logout();
+    res.redirect('/login');
+  }
+}
---
+++ routes/donors.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var models = require('../models');
+var donors = require('../controllers/donors');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+require('../controllers/passport/strategies')(passport);
+var router = express.Router();
+
+router.get('/profile', donors.profile)
+// router.post('/transaction_signup', donors.transaction_signup)
+router.get('/register', donors.register)
+// router.post('/register', passport.authenticate('registrationStrategy', {successRedirect: '/donor/profile', failureRedirect: '/donor/register'}))
+router.get('/logout', donors.logout)
+
+module.exports = router
