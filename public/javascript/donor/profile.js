74536d7a29e2abc9c08c37e4c076299d4f5d7788
JimmyMorrill <Jimbo@Jamess-MacBook-Pro-2.local>
2016-12-20 15:13:18 +0000

    neatened donations list, introduced a scroll

--- public/javascript/donor/profile.js
+++ public/javascript/donor/profile.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   var initialsCircle = '.initials.circle'
   for(var i = 0; i < 10; i++) {
     newInitialsCircle = initialsCircle + '.' + i.toString();
-    if(i % 3 == 1) {
+    if(i % 3 == 2) {
       $(newInitialsCircle).css('background-color', '#00da8d')
-    } else if (i % 3 == 2) {
+    } else if (i % 3 == 0) {
       $(newInitialsCircle).css('background-color', '#7068FF')
     } else {
       $(newInitialsCircle).css('background-color', '#FFC300')
